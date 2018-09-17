6f8df733a94030d31a6930ca7377b2ad3bc16995
Shayne Wang <1614565666@qq.com>
2018-09-17 15:10:33 +0800

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+const Koa = require("koa")
+const static = require("koa-static")
+
+const app = new Koa()
+
+app.use(static(__dirname + '/www'));
+
+app.listen(9001, function () {
+    console.log("启动成功a ")
+})
+``
