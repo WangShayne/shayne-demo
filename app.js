b225821f0530e1431518af91af9054a9c67f82ed
Shayne Wang <1614565666@qq.com>
2018-09-18 09:16:38 +0800

    change router for test

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const Koa = require("koa")
 const static = require("koa-static")
+const Router = require("koa-router")
 
 const app = new Koa()
+const router = new Router()
+
+app.post("/webhook",function(ctx,next){
+    ctx.body="接收到请求"
+    next()
+})
+app.use(router.routes())
 
 app.use(static(__dirname + '/www'));
 
 app.listen(9001, function () {
-    console.log("启动成功a ")
+    console.log("启动成功")
 })
