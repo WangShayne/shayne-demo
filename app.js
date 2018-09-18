016c524b993e06dc868f9c2b28b3e22a35ec158a
Shayne Wang <1614565666@qq.com>
2018-09-18 10:04:54 +0800

    test for response header

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const Koa = require("koa")
 const static = require("koa-static")
 const Router = require("koa-router")
 const shelljs = require("shelljs")
-
+const token = "S4A^87pZ3=}&QP?r"
 const app = new Koa()
 const router = new Router()
 
 
 router.post("/webhook",function(ctx,next){
+    console.log(ctx.headers)
     console.log(ctx.request)
     ctx.body="接收到请求"
     next()
